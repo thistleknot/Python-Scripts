57d05f0a2bbe333e216faaf1a36c6bad82412f97
Turning out data tricks since 2006! <laferrierejc@gmail.com>
2018-10-07 20:14:56 -0700

    1st Model

--- Parse.py
+++ Parse.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import sklearn.model_selection as ms
+import statsmodels.api as sm
 #from numpy import percentile
 
 input_file = "C:/Users/user/Documents/plots/parsed.csv"
@@ -8,10 +9,16 @@ input_file = "C:/Users/user/Documents/plots/parsed.csv"
 df = pd.read_csv(input_file, header = 0)
 df
 
-X, y = df.iloc[:, :-1], df.iloc[:, -1]
+X, y = df.iloc[:, :-2], df.iloc[:, -2]
+
+x = df.iloc[:, -3]
+
+x
 
 X
 
 y
 
-X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size=0.25, random_state=42)
+model = sm.OLS(y, x).fit()
+
+model.summary()
