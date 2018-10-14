48c3d0277aa2969c44a7d1a60360a4814f1dc407
Turning out data tricks since 2006! <laferrierejc@gmail.com>
2018-10-13 23:35:46 -0700

    updated script and location of parsed.csv
    fixed parsed.csv

--- Parse.py
+++ Parse.py
@@ -4,14 +4,14 @@ import sklearn.model_selection as ms
 import statsmodels.api as sm
 #from numpy import percentile
 
-input_file = "C:/Users/user/Documents/plots/parsed.csv"
+input_file = "C:/Users/user/Documents/Python Scripts/parsed.csv"
 
 df = pd.read_csv(input_file, header = 0)
 df
 
-X, y = df.iloc[:, :-2], df.iloc[:, -2]
+X, y = df.iloc[:, :-2], df.iloc[:, -1]
 
-x = df.iloc[:, -3]
+x = df.iloc[:, 1:51]
 
 x
 
