bd13bc17a43d2457a9d9efa38d21867db13a116a
Turning out data tricks since 2006! <laferrierejc@gmail.com>
2018-10-15 19:54:14 -0700

    working on getting prediction working
    my training and validation data is a mess

--- Parse.py
+++ Parse.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+from sklearn import datasets, linear_model, metrics 
+
 import numpy as np
 import pandas as pd
 import sklearn.model_selection as ms
@@ -46,33 +50,55 @@ tx
 
 #offset
 tx_lagged = tx.shift(+1)
+vx_lagged = X.shift(+1)
 ty_lagged = ty.shift(+1)
+vy_lagged = ty.shift(+1)
 ty_future = ty.shift(-1)
-
+vy_future = ty.shift(-1)
 
 #yields
 
 tx_yield = (tx-tx_lagged)/tx_lagged
+vx_yield = (vx-vx_lagged)/vx_lagged
 ty_yield = (ty-ty_lagged)/ty_lagged
+vy_yield = (vy-vy_lagged)/vy_lagged
 ty_future_yield = (ty_future-ty)/ty
+vy_future_yield = (vy_future-vy)/vy
 
 #validate
 #ty_yield
-
 
 #tx_lagged
 
 #df1 = df.loc[:, ['date', 'CPIAUCSL']]df1 = df.loc[
 
 XNew = pd.concat([tx, tx_yield, ty, ty_yield], axis=1)
+vXNew = pd.concat([vx, vx_yield, vy, vy_yield], axis=1)
 YNew = ty_future_yield
+vYNew = vy_future_yield
 
-model = sm.OLS(ty, tx).fit()
+#training
+#model = sm.OLS(ty, tx).fit()
 modelWLag = sm.OLS(YNew.loc[109:270], XNew.loc[109:270,]).fit()
 
-model.summary()
+reg = linear_model.LinearRegression() 
+  
+# train the model using the training sets 
+#reg.fit(YNew.loc[109:270], tx.loc[109:270,])
+
+#model.summary()
 #ty_future_yield
 
 modelWLag.summary()
+#modelWLag.params
+
+modelWLag.predict(XNew.loc[271:327])
+#vx_lagged
+vx_yield
+#modelWLag.fit()
+#results = model.fit()
+#results
+
+#modelWLag.fit()
 #YNew.loc[109:,]
 #XNew.loc[109:,]
