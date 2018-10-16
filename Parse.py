33c9bd0b8e5c5781cdb4f272fdc3bff02ac14436
Turning out data tricks since 2006! <laferrierejc@gmail.com>
2018-10-15 21:13:10 -0700

    lots of cleanup
    cleaned up the dataset to do the lag transformations at the beginning
    
    cut out date, i would like to transform it to a margin value

--- Parse.py
+++ Parse.py
@@ -17,88 +17,36 @@ df = pd.read_csv(input_file, header = 0)
 split=int(len(df.index))/2
 
 #DCOILWTICO was not significant, most likely due to present of cpiaucsl
-X = df.loc[108:, ['date','CPIAUCSL','PSAVERT','GDPC1','DGS10','UMCSENT','EMRATIO','POPTOTUSA647NWDB','TTLHH','MEHOINUSA672N']]
+x = df.loc[0:, ['date','CPIAUCSL','PSAVERT','GDPC1','DGS10','UMCSENT','EMRATIO','POPTOTUSA647NWDB','TTLHH','MEHOINUSA672N']]
 
-y = df.loc[108:, ['SPCS20RSA']]
-
-#108 to just shy of last
-
-#training
-tx = X.iloc[0:int(split), 1:10]
-
-#df1 = df.loc[:, ['date','PSAVERT','GDPC1','DGS10','UMCSENT','EMRATIO','POPTOTUSA647NWDB','TTLHH','MEHOINUSA672N']]
-
-
-ty = y.iloc[0:int(split), ]
-
-#validation
-vx = X.iloc[int(split+1):,1:10]
-
-#print
-vy = y.iloc[int(split+1):,1:10]
-
-#print
-vx
-ty
-tx
-#split
-#ty = df.iloc[0:int(split), -1]
-
-#tX = X.iloc[0:(split+1):, 0:52]
-
+y = df.loc[0:, ['CSUSHPINSA']]
 #y
 
 #offset
-tx_lagged = tx.shift(+1)
-vx_lagged = X.shift(+1)
-ty_lagged = ty.shift(+1)
-vy_lagged = ty.shift(+1)
-ty_future = ty.shift(-1)
-vy_future = ty.shift(-1)
+x_lagged = X.shift(+1)
+y_lagged = y.shift(+1)
 
-#yields
+y_future = y.shift(-1)
 
-tx_yield = (tx-tx_lagged)/tx_lagged
-vx_yield = (vx-vx_lagged)/vx_lagged
-ty_yield = (ty-ty_lagged)/ty_lagged
-vy_yield = (vy-vy_lagged)/vy_lagged
-ty_future_yield = (ty_future-ty)/ty
-vy_future_yield = (vy_future-vy)/vy
+# 1: skip 1st row
+# :-1 skip last row
+# ,1: skip 1st column
+    
+x_yield = (x.iloc[1:-1,1:]-x_lagged.iloc[1:-1,1:])/x_lagged.iloc[1:-1,1:]
 
-#validate
-#ty_yield
+y_yield = (y.iloc[1:-1,]-y_lagged.iloc[1:-1,])/y_lagged.iloc[1:-1,]
+#y_future_yield = ()
 
-#tx_lagged
+y_future_yield = (y_future.iloc[1:-1,]-y.iloc[1:-1,])/y_future.iloc[1:-1,]
+y_future_yield
 
-#df1 = df.loc[:, ['date', 'CPIAUCSL']]df1 = df.loc[
+x_and_y_with_yields = pd.concat([x.iloc[1:-1,1:], x_yield, y.iloc[1:-1,], y_yield], axis=1)
+x_and_y_with_yields
 
-XNew = pd.concat([tx, tx_yield, ty, ty_yield], axis=1)
-vXNew = pd.concat([vx, vx_yield, vy, vy_yield], axis=1)
-YNew = ty_future_yield
-vYNew = vy_future_yield
+#XNew = pd.concat([tx, tx_yield, ty, ty_yield], axis=1)
+#vXNew = pd.concat([vx.loc[273:327], vx_yield.loc[273:327], vy.loc[273:327], vy_yield.loc[273:327]], axis=1)
 
-#training
-#model = sm.OLS(ty, tx).fit()
-modelWLag = sm.OLS(YNew.loc[109:270], XNew.loc[109:270,]).fit()
-
-reg = linear_model.LinearRegression() 
-  
-# train the model using the training sets 
-#reg.fit(YNew.loc[109:270], tx.loc[109:270,])
-
-#model.summary()
-#ty_future_yield
-
-modelWLag.summary()
+#modelWLag.summary()
 #modelWLag.params
 
-modelWLag.predict(XNew.loc[271:327])
-#vx_lagged
-vx_yield
-#modelWLag.fit()
-#results = model.fit()
-#results
-
-#modelWLag.fit()
-#YNew.loc[109:,]
-#XNew.loc[109:,]
+#modelWLag.predict(vXNew)
