7e0162202ebf221f19f8d24f960a0db8eeaf8087
Turning out data tricks since 2006! <laferrierejc@gmail.com>
2018-10-16 18:35:15 -0700

    scikit modeling applied
    I'm having a hard time exporting the data, but I have multiple simulations running over the data deriving it's own accuracy results.

--- Parse.py
+++ Parse.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
 from sklearn import datasets, linear_model, metrics 
+from sklearn.model_selection import train_test_split
 
 import numpy as np
 import pandas as pd
 import sklearn.model_selection as ms
 import statsmodels.api as sm
+
 #from numpy import percentile
 
 input_file = "C:/Users/user/Documents/Python Scripts/parsed.csv"
@@ -23,7 +25,7 @@ y = df.loc[0:, ['CSUSHPINSA']]
 #y
 
 #offset
-x_lagged = X.shift(+1)
+x_lagged = x.shift(+1)
 y_lagged = y.shift(+1)
 
 y_future = y.shift(-1)
@@ -52,6 +54,29 @@ model = sm.OLS(y_future_yield.loc[1:int(split+1)], x_and_y_with_yields.loc[1:int
 results = pd.concat([model.predict(x_and_y_with_yields.loc[int(split+2):]), y_future_yield[int(split+1):]], axis=1)
 results
 
+model.params
+model.summary()
+
+#scikit
+#https://towardsdatascience.com/train-test-split-and-cross-validation-in-python-80b61beca4b6
+X_train, X_test, y_train, y_test = train_test_split(x_and_y_with_yields.loc[1:int(split+1)], y_future_yield.loc[1:int(split+1)], test_size=0.2)
+
+lm = linear_model.LinearRegression()
+
+model = lm.fit(X_train, y_train)
+predictions = lm.predict(X_test)
+
+predictions.shape[0]
+y_test.shape[0]
+#pd.concat([predictions,y_test],axis=1)
+plt.scatter(y_test, predictions)
+plt.xlabel("True Values")
+plt.ylabel("Predictions")
+
+print ("Score:", model.score(X_test, y_test))
+
+#type(y_test)
+#type(predictions)
+#predictions[0:,0:].tolist()
 #model.params
 #model.summary()
-
