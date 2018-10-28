361ece9f1c1fb1e153bf9e3d8fe6b16ad1f03d8c
Turning out data tricks since 2006! <laferrierejc@gmail.com>
2018-10-28 10:02:31 -0700

    on my way to state vector machines

--- Parse.py
+++ Parse.py
@@ -53,30 +53,50 @@ set1 = pd.concat([x,xLagged,x*xLagged,y,yYield], axis=1)
 
 #[1:,][:-1]
 #remove top and bottom row
-X_train, X_test, y_train, y_test = train_test_split(set1.loc[1:,][:-1], yFutureYield.loc[1:,][:-1], test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(set1.loc[1:,][:-1], yFutureYield.loc[1:,][:-1], test_size=0.25)
 
 #should be checking and flagging both columns if na is found in any
 model_scikit = lm.fit(X_train.dropna(axis=1, how='all'), y_train)
 predictions = lm.predict(X_test.dropna(axis=1, how='all'))
 
 model_training = sm.OLS(y_train,X_train,missing = 'drop').fit()
+model_training2 = sm.OLS(model_training.predict(X_train),y_train,missing='drop').fit()
 print(model_training.summary())
+print("Prediction Training")
+print(model_training2.summary())
 
 print(pd.concat([model_training.predict(X_test),y_test],axis=1))
 
 print ("Train Score:", model_scikit.score(X_train.dropna(axis=1, how='all'), y_train))
 print ("Test Score:", model_scikit.score(X_test.dropna(axis=1, how='all'), y_test))
-dir(model_training)
-model_testing = sm.OLS(model_training.predict(X_test),y_test,missing='drop').fit()
-model_testing.summary()
+
+predicted = model_training.predict(X_test)
+model_testing = sm.OLS(predicted,y_test,missing='drop').fit()
+
+print("Prediction Testing")
+print(model_testing.summary())
+
+#modelAppliedToTesting
+#model_testing2 = sm.OLS(y_test,X_test,missing = 'drop').fit()
+#print(model_testing2.summary())
+
+#print("Factors applied to Training")
+#print(model_testing2.summary())
+
+delta = predicted.values.flatten() - y_test.values.flatten()
 
 plt.scatter(y_test, model_training.predict(X_test))
 plt.xlabel("True Values")
 plt.ylabel("Predictions")
 
-#http://scikit-learn.org/stable/modules/cross_validation.html
+predicted = np.array(model_training.predict(X_test)).flatten()
+deltas = predicted-yt
 
-#np.array(X_train).flatten()
-#np.array(y_train).flatten()
+pd.DataFrame(deltas).hist()
+#pd.DataFrame.from_records(deltas)
 
-#clf = svm.SVC(kernel='linear', C=1).fit(np.array(X_train).flatten(), np.array(y_train).flatten())
+rlf = svm.SVR(kernel='linear', C=1).fit(X_train, y_train.values.flatten())
+print(rlf.score(X_test, y_test))
+
+
+#deltas
