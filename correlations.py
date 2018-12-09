c1b4be11317a0c514826c09f93fd851f95029418
Turning out data tricks since 2006! <laferrierejc@gmail.com>
2018-12-09 11:06:09 -0800

    removed stepwise

--- correlations.py
+++ correlations.py
@@ -6,62 +6,11 @@ from sklearn import datasets, linear_model, metrics
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-input_file = "output_test.csv"
+input_file = "output.csv"
 
 #https://stackoverflow.com/questions/36519086/pandas-how-to-get-rid-of-unnamed-column-in-a-dataframe/36519122
 df = pd.read_csv(input_file, header = 0, index_col=0)
 
-#def train_test_split2(X, y)
-
-def stepwise_selection(X, y, 
-                       initial_list=[], 
-                       threshold_in=0.01, 
-                       threshold_out = 0.05, 
-                       verbose=True):
-    """ Perform a forward-backward feature selection 
-    based on p-value from statsmodels.api.OLS
-    Arguments:
-        X - pandas.DataFrame with candidate features
-        y - list-like with the target
-        initial_list - list of features to start with (column names of X)
-        threshold_in - include a feature if its p-value < threshold_in
-        threshold_out - exclude a feature if its p-value > threshold_out
-        verbose - whether to print the sequence of inclusions and exclusions
-    Returns: list of selected features 
-    Always set threshold_in < threshold_out to avoid infinite looping.
-    See https://en.wikipedia.org/wiki/Stepwise_regression for the details
-    """
-    included = list(initial_list)
-    while True:
-        changed=False
-        # forward step
-        excluded = list(set(X.columns)-set(included))
-        new_pval = pd.Series(index=excluded)
-        for new_column in excluded:
-            model = sm.OLS(y, sm.add_constant(pd.DataFrame(X[included+[new_column]]))).fit()
-            new_pval[new_column] = model.pvalues[new_column]
-        best_pval = new_pval.min()
-        if best_pval < threshold_in:
-            best_feature = new_pval.idxmin()
-            included.append(best_feature)
-            changed=True
-            if verbose:
-                print('Add  {:30} with p-value {:.6}'.format(best_feature, best_pval))
-
-        # backward step
-        model = sm.OLS(y, sm.add_constant(pd.DataFrame(X[included]))).fit()
-        # use all coefs except intercept
-        pvalues = model.pvalues.iloc[1:]
-        worst_pval = pvalues.max() # null if pvalues is empty
-        if worst_pval > threshold_out:
-            changed=True
-            worst_feature = pvalues.idxmax()
-            included.remove(worst_feature)
-            if verbose:
-                print('Drop {:30} with p-value {:.6}'.format(worst_feature, worst_pval))
-        if not changed:
-            break
-    return included
 
 #x = df.drop(columns=['date','CSUSHPINSA']).sample(20, axis=1)
 date = df.loc[0:,['test2_z.date']]
@@ -72,7 +21,6 @@ date = df.loc[0:,['test2_z.date']]
 x = df.drop(columns=['test2_z.date','CSUSHPINSA','FGCCSAQ027S','future'])
 y = df.loc[0:,['CSUSHPINSA']]
 
-#dateLagged = date.shift(+1)
 xLagged = x.shift(+1)
 yLagged = y.shift(+1)
 yFuture = y.shift(-1)
@@ -96,13 +44,20 @@ x.columns = ['x_' + str(col) for col in x.columns]
 yYield.columns = ['yYield_' + str(col) for col in yYield.columns]
 yFYield.columns = ['yFYield_' + str(col) for col in yFYield.columns]
 
+#https://chrisalbon.com/python/data_wrangling/pandas_create_column_using_conditional/
+#used for quarters
+df_x = pd.DataFrame(date)
 
-result = pd.concat([date, x, xYield, y, yYield, yFYield], axis=1, sort=False)
+#df_x['Q1'] = np.where(==12, 1, 0)
+
+
+
+
+#x['Q1'] = np.where(date['date']>=50, 1, 0)
+
+result = pd.concat([date, df_x, xYield, y, yYield, yFYield], axis=1, sort=False)
 #replace inf's with 0
 result.replace(np.inf, 0, inplace=True)
-
-
-
 
 result.to_csv("output.csv", sep=',')
 #plt.matshow(result.corr())
@@ -113,8 +68,20 @@ corr = result.corr()
 #matches formula here: https://libguides.library.kent.edu/SPSS/PearsonCorr
 corr.to_csv("corr.csv", sep=',')
 
-print(corr)
+
+
+#print(corr)
 #corr.style.background_gradient(highlight_rows, axis = 0)
 #corr.style.apply(highlight_rows, axis = 0)
 #corr.style.background_gradient()
+
+
+
+#dateLagged = date.shift(+1)
+#bins = [ 1, 5, 10, 25, 50, 100]
+#df['binned'] = pd.cut(df['percentage'], bins)
+
+
+#pandas.Series.dt.month(date)
+
 
