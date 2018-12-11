f07870a702a4a8f044f4157e51f92367c9e1e4d9
Turning out data tricks since 2006! <laferrierejc@gmail.com>
2018-12-10 20:53:59 -0800

    correlations

--- correlations.py
+++ correlations.py
@@ -5,6 +5,7 @@ import statsmodels.api as sm
 from sklearn import datasets, linear_model, metrics 
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+import datetime
 
 input_file = "output_test.csv"
 
@@ -13,7 +14,10 @@ df = pd.read_csv(input_file, header = 0, index_col=0)
 
 
 #x = df.drop(columns=['date','CSUSHPINSA']).sample(20, axis=1)
-date = df.loc[0:,['test2_z.date']]
+#date = df.loc[0:,['test2_z.date']]
+
+#quarters = (date.month-1)//3
+
 
 #append if desire a sample
 #.sample(50, axis=1)
@@ -50,9 +54,6 @@ yFYield.columns = ['yFYield_' + str(col) for col in yFYield.columns]
 
 #df_x['Q1'] = np.where(==12, 1, 0)
 
-
-
-
 #x['Q1'] = np.where(date['date']>=50, 1, 0)
 
 result = pd.concat([date, xYield, y, yYield, yFYield], axis=1, sort=False)
@@ -63,13 +64,57 @@ result.to_csv("output.csv", sep=',')
 #plt.matshow(result.corr())
 #https://stackoverflow.com/questions/29432629/correlation-matrix-using-pandas
 
+fieldOfInterest='yFYield_CSUSHPINSA'
+
 corr = result.corr()
 #matches corr formula in excel
 #matches formula here: https://libguides.library.kent.edu/SPSS/PearsonCorr
 corr.to_csv("corr.csv", sep=',')
+#abs(corr)
+upperLimit = max(abs(corr[fieldOfInterest]).mean(),abs(corr[fieldOfInterest]).median())
+#upperLimit
 
+#https://stackoverflow.com/questions/42613467/how-to-select-all-rows-which-contain-values-greater-than-a-threshold
+#df[df['column'] == value]
 
+#drop yFYield_CSUSHPINSA from output, but, filter on this column by upperLimit
+#upper=corr.drop(columns=['yFYield_CSUSHPINSA'])[abs(corr['yFYield_CSUSHPINSA']) > upperLimit]
+upper=corr[fieldOfInterest][abs(corr[fieldOfInterest]) > upperLimit]
 
+#https://stackoverflow.com/questions/26640145/python-pandas-how-to-get-the-row-names-from-index-of-a-dataframe
+upperList = upper.axes[0].tolist()
+
+#filters columns
+upperSet=corr[upperList]
+#upper.loc[upperList]
+#upperSet[abs(corr[fieldOfInterest]) > upperLimit]
+
+#column means
+lowerLimit = min(abs(upperSet).mean().mean(),abs(upperSet).median().median())
+
+#lower1 = upperSet[[]]
+
+#lowerLimit
+LowerList1 = abs(upperSet).mean()[abs(upperSet).mean() < lowerLimit].axes[0].tolist()
+LowerList2 = abs(upperSet).median()[abs(upperSet).median() < lowerLimit].axes[0].tolist()
+#LowerList2
+#LowerList2
+
+#final List
+finalSet=set(LowerList1)&set(LowerList2)
+
+corr.loc[finalSet][list(finalSet)]
+#common
+
+#list(set(list1).intersection(list2))
+
+#lowerList = lower1.axes[0].tolist()
+#lowerList
+#lowerSet=upperSet[lowerList]
+
+#upper=corr[fieldOfInterest][abs(corr[fieldOfInterest]) > upperLimit]
+
+#abs(upperSet).mean().mean()
 #print(corr)
 #corr.style.background_gradient(highlight_rows, axis = 0)
 #corr.style.apply(highlight_rows, axis = 0)
@@ -81,8 +126,13 @@ corr.to_csv("corr.csv", sep=',')
 #bins = [ 1, 5, 10, 25, 50, 100]
 #df['binned'] = pd.cut(df['percentage'], bins)
 
+#dir(date)
 
+#date.to_datetime(date.date)
 #pandas.Series.dt.month(date)
+
+#https://stackoverflow.com/questions/26105804/extract-month-from-date-in-python/26105888
+#datetime.datetime.strptime(date, "%Y-%m-%d")
 
 
 #result
